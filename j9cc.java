6aeb402ccac0596c147e333bcac1232a067cdb53
Masateru <lion0024.gmail.com>
2020-04-02 21:28:11 +0900

    足し算と引き算を追加

--- j9cc.java
+++ j9cc.java
@@ -1,3 +1,6 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 class j9cc{
   public static void main(String[] args){
     if (args.length != 1){
@@ -5,10 +8,34 @@ class j9cc{
       return;
     }
 
+    String str = args[0];
+    String[] num = str.split("[+-]");
+    String[] op = str.split("[0-9]+");
+
     System.out.println(".intel_syntax noprefix");
     System.out.println(".global main");
     System.out.println("main:");
-    System.out.println("  mov rax, " + args[0]);
+    System.out.println("  mov rax, " + num[0]);
+    
+    // op配列の0に空白が入ってしまっているため要修正
+    int num_i = 0;
+    for (int i = 1; i < op.length; i++){
+      if (op[i].equals("+")){
+        num_i++;
+        System.out.println("  add rax, " + num[num_i]);
+        continue;
+      }
+
+      if (op[i].equals("-")){
+        num_i++;
+        System.out.println("  sub rax, " + num[num_i]);
+        continue;
+      }
+
+      System.err.println("unexpected character: " + op[i]);
+      return;
+    }
+
     System.out.println("  ret");
     return;
   }
